b5ec3a47fa997aa7befaa1a3f3c5b3ee582459ed
nguyenthaitan <nguyenthaitan9@gmail.com>
2020-12-10 11:58:51 +0700

    fix bug

--- index.js
+++ index.js
@@ -1,59 +1,62 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5001;
-const hbs = require('express-handlebars');
+const hbs = require("express-handlebars");
 app.engine(
-  'hbs',
+  "hbs",
   hbs({
-    extname: 'hbs',
-    defaultLayout: 'layout',
-    layoutsDir: __dirname + '/views/layouts/',
-    partialsDir: __dirname + '/views/partials/',
+    extname: "hbs",
+    defaultLayout: "layout",
+    layoutsDir: __dirname + "/views/layouts/",
+    partialsDir: __dirname + "/views/partials/",
     runtimeOptions: {
       allowProtoPropertiesByDefault: true,
     },
   })
 );
 
-app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'hbs');
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "hbs");
 
-app.get('/', (req, res) => {
-  res.render('index', { author: 'Team12' });
+app.get("/", (req, res) => {
+  res.render("index", { author: "Team12" });
 });
 
-app.get('/createTables', (req, res) => {
-  let models = require('./models');
+app.get("/createTables", (req, res) => {
+  let models = require("./models");
   models.sequelize.sync().then(() => {
-    res.send('Tables created!');
+    res.send("Tables created!");
   });
 });
 
-app.get('/recipes', (req, res) => {
-  let recipeController = require('./controllers/recipeController');
+app.get("/recipes", (req, res) => {
+  let recipeController = require("./controllers/recipeController");
   recipeController.getAll().then((data) => {
     console.log(data);
-    res.render('recipes', {
-      author: 'Nguyen Dang Huynh Long - 18127136',
+    res.render("recipes", {
+      author: "Nguyen Dang Huynh Long - 18127136",
       list: data,
       n: 2,
     });
   });
 });
 
-app.get('/demo', (req, res) => {
-  let recipeController = require('./controllers/recipeController');
+app.get("/demo", (req, res) => {
+  let recipeController = require("./controllers/recipeController");
   recipeController.getOne(1).then((data) => {
     console.log(data);
     res.send(data);
   });
 });
 
-app.get('/feature/:recipeId', async (req, res) => {
-  let recipeController = require('./controllers/recipeController');
+app.get("/feature/:recipeId", async (req, res) => {
+  let recipeController = require("./controllers/recipeController");
   recipeController.getOne(req.params.recipeId).then((data) => {
     console.log(data);
-    res.render("features", { data: data, author: "18127269 - Nguyễn Thái Tân" });
+    res.render("features", {
+      data: data,
+      author: "18127269 - Nguyễn Thái Tân",
+    });
   });
 });
 
